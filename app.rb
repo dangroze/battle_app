28c9a559f626f9d6105305dff80924e22eeabc02
dangroze <dangroze@gmail.com>
2019-01-22 14:47:02 +0000

    Added infrastructure feature test -passed

--- app.rb
+++ app.rb
@@ -1,8 +1,12 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
-  get '/' do
+  get 'greeting' do
     'Hello Battle!'
+  end
+
+  get '/' do
+    'Testing infrastructure working!'
   end
 
   run! if app_file == $0
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,16 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+# require our Sinatra app file
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+# tell Capybara about our app class
+Capybara.app = Battle
+ENV['RACK_ENV'] = 'test'
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
