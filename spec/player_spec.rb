194bc9fd6688cc8bc28aaef3143abc91d819edf5
dangroze <dangroze@gmail.com>
2019-01-24 17:29:07 +0000

    Added tests for hitpoints, attack and receive_damage

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,9 +1,28 @@
 require 'player'
 
-feature 'Player name' do
-  scenario 'it returns player name' do
-    sign_in_and_play
+RSpec.describe Player do
+  it 'returns player name' do
     player1 = Player.new("Dan")
     expect(player1.name).to eq("Dan")
+  end
+  describe '#hitpoints' do
+    it 'returns the hitpoints' do
+      player1 = Player.new('Krzys')
+      expect(player1.hitpoints).to eq(Player::DEFAULT_HITPOINTS)
+    end
+  end
+  describe '#attack' do
+    it 'damages the player' do
+      player1 = Player.new('Krzys')
+      player2 = Player.new('Dan')
+      expect(player2).to receive(:receive_damage)
+      player1.attack(player2)
+    end
+  end
+  describe '#receive_damage' do
+    it 'reduces the hitpoints' do
+      player2 = Player.new('Dan')
+      expect{player2.receive_damage}.to change{player2.hitpoints}.by(-10)
+    end
   end
 end
